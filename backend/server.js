998208f9a37b217c8df1fbacaf8634e8e167f426
Peter Ngugi <86372952+muirurii@users.noreply.github.com>
2023-02-17 21:27:11 +0300

    Added origin for deployed site

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const allowedOrigins = [
     // "http://localhost:3000",
     // "http://localhost:5000",
-    "https://advancedchat.onrender.com/",
+    "https://advancedchat.onrender.com",
     // undefined
 ];
 
